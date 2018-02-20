scottjames/garmintools

commit 8571a79fdf148bc6881a435303590d0317f5efd1

garmin_gchart: Fix crash on wrong input
gchart_e_encode crashes if values < 0 or > 4095

Fixes https://code.google.com/archive/p/garmintools/issues/13
Patch generated from file attached to that issue.

src/garmin_gchart.c
@@ -83,7 +83,7 @@ gchart_e_encode ( float32 num, float32 max, char * str )
     str[0] = '_';
     str[1] = '_';
     str[2]= '\0';
-  }
+  } else {
   str[0]=gchart_e_encode_single((int)num/64);
   str[1]=gchart_e_encode_single(num - ((int)(num/64)*64) );
   str[2]='\0';

commit 3e931ebbad7fdc5babdc1b3ad29f315ffc896a46

Fix compile issue
Introduced with 8571a79fdf148bc6881a435303590d0317f5efd1

src/garmin_gchart.c
@@ -84,9 +84,10 @@ gchart_e_encode ( float32 num, float32 max, char * str )
     str[1] = '_';
     str[2]= '\0';
   } else {
-  str[0]=gchart_e_encode_single((int)num/64);
-  str[1]=gchart_e_encode_single(num - ((int)(num/64)*64) );
-  str[2]='\0';
+    str[0]=gchart_e_encode_single((int)num/64);
+    str[1]=gchart_e_encode_single(num - ((int)(num/64)*64) );
+    str[2]='\0';
+  }
 }
 
 